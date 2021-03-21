fd6dd2e669a23c9349f10b32bc6af3a30e66dedb
ruskab <aq0866sw1343de>
2021-03-21 18:31:20 +0100

    initial commit

---
+++ ostyt/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+import {name, version} from '../../package.json';
+
+export const environment = {
+  production: true,
+  NAME: name,
+  VERSION: version,
+  REST_CORE: 'https://ost-yt.herokuapp.com',
+};
